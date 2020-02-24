2d1ba5fffb8ecf4ade0197f464060ff256efd1ad
Mostafa Gad <Gad.mostafa@gmail.com>
2020-02-24 02:29:07 +0100

    remove current

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import { Row, Col, Button } from "reactstrap";
 import Map from "./Map";
-import api from "../api";
 
 export interface HomeProps {}
 
 const Home: React.SFC<HomeProps> = () => {
   const [location, setLocation] = useState("");
   const [mapInfo, setMapInfo] = useState("");
-
-  const getNavigator = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const currentCity = await api.getNearByCities(
-          position.coords.latitude,
-          position.coords.longitude,
-        );
-        setMapInfo(currentCity.geonames[0].name);
-      });
-    }
-  };
 
   return (
     <Row
@@ -51,16 +38,6 @@ const Home: React.SFC<HomeProps> = () => {
             className="ml-2  btn-dark"
           >
             Search
-          </Button>
-          <Button
-            onClick={() => {
-              setMapInfo("");
-              setLocation("");
-              getNavigator();
-            }}
-            className="ml-2  btn-danger"
-          >
-            Get Current
           </Button>
         </div>
 
